9695e5fce236a6bbdb30eb670260b7dcd258a9f8
lijinze <2533755010@qq.com>
2020-07-16 22:10:11 +0800

    init rep

---
+++ src/main/java/com/curiosity/blog/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.curiosity.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @description:
+ * @author: lijinze
+ * @createDate: 2020/7/16
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("msg", name);
+
+        return "hello";
+    }
+
+}
